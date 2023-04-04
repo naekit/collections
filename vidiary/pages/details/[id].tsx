13a6cddce0e33a186142093ae5be054a532fbdf2
naekit <mykyta.onishchuck@code.berlin>
2023-04-04 12:03:26 -0400

    added video details page and play functionality to videos

--- vidiary/pages/details/[id].tsx
+++ vidiary/pages/details/[id].tsx
@@ -11,9 +11,51 @@ import { BASE_URL } from "@/utils"
 import { Video } from "@/types"
 
 const Details = ({ postDetails }: { postDetails: Video }) => {
+	const [post, setPost] = useState<Video>(postDetails)
+	const videoRef = React.useRef<HTMLVideoElement>(null)
+	const [isMuted, setIsMuted] = useState<boolean>(false)
+	const [isPlaying, setIsPlaying] = useState<boolean>(false)
+
+	if (!post) return <h1>404</h1>
+
+	const onVideoClick = () => {
+		if (!videoRef.current) {
+			return
+		}
+		if (videoRef.current.paused) {
+			videoRef.current.play()
+			setIsPlaying(true)
+		} else {
+			videoRef.current.pause()
+			setIsPlaying(false)
+		}
+	}
+
 	return (
-		<div>
-			<h1>Details</h1>
+		<div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+			<div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blur">
+				<div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+					<p>
+						<MdOutlineCancel className="text-2xl lg:text-3xl text-white" />
+					</p>
+				</div>
+				<div className="relative">
+					<div className="lg:h-[100vh] h-[60vh]">
+						<video
+							ref={videoRef}
+							loop
+							onClick={onVideoClick}
+							src={post.video.asset.url}
+							className="h-full"
+						></video>
+					</div>
+					{!isPlaying && (
+						<button onClick={onVideoClick}>
+							<BsFillPlayFill className="text-4xl lg:text-6xl text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50" />
+						</button>
+					)}
+				</div>
+			</div>
 		</div>
 	)
 }
@@ -29,7 +71,7 @@ export const getServerSideProps = async ({
 	console.log(data)
 	return {
 		props: {
-			data,
+			postDetails: data,
 		},
 	}
 }
