bd3a6a7c9965eca672a2132b8aa2d3ef177d402e
naekit <mykyta.onishchuck@code.berlin>
2023-04-05 12:20:07 -0400

    added filter by topic

--- vidiary/pages/index.tsx
+++ vidiary/pages/index.tsx
@@ -31,7 +31,9 @@ export default function Home({ videos }: Props) {
 							<VideoCard post={video} key={video._id} />
 						))
 					) : (
-						<NoResults text="No videos found" />
+						<div className="mt-20">
+							<NoResults text="No videos found" />
+						</div>
 					)}
 				</div>
 			</main>
@@ -39,12 +41,21 @@ export default function Home({ videos }: Props) {
 	)
 }
 
-export const getServerSideProps = async () => {
-	const { data } = await axios.get(`${BASE_URL}/api/post`)
+export const getServerSideProps = async ({
+	query: { topic },
+}: {
+	query: { topic: string }
+}) => {
+	let response
+	if (topic) {
+		response = await axios.get(`${BASE_URL}/api/discover/${topic}`)
+	} else {
+		response = await axios.get(`${BASE_URL}/api/post`)
+	}
 
 	return {
 		props: {
-			videos: data,
+			videos: response.data,
 		},
 	}
 }
